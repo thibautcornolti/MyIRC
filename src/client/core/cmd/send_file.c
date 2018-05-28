ba0e42ef70f0751574827ebf80934887da5a3c95
Thibaut Cornolti <thibaut@cornolti.eu>
2018-05-28 13:10:13 +0200

    filename

--- src/client/core/cmd/send_file.c
+++ src/client/core/cmd/send_file.c
@@ -7,6 +7,18 @@
 
 #include "client.h"
 
+static char *cmd_send_fname(char *or)
+{
+	int i = -1;
+	int s = 0;
+
+	while (or[++i]) {
+		if (or[i] == '/' && or[i + 1])
+			s = i;
+	}
+	return (or + s + 1);
+}
+
 bool cmd_send_file(sess_t *sess, char *line)
 {
 	int fd;
@@ -15,14 +27,17 @@ bool cmd_send_file(sess_t *sess, char *line)
 
 	if (!cmd)
 		return (false);
-	if (len_array((void **) cmd) >= 2) {
+	if (len_array((void **)cmd) >= 2) {
 		fd = open(cmd[1], O_RDONLY);
-		if (fd == -1 || !send_file(sess, fd, cmd[0], cmd[1]))
-			sess->printfChan(sess, "master", "Cannot send file "
-			"\"%s\"", cmd[1]);
+		if (fd == -1 ||
+			!send_file(sess, fd, cmd[0], cmd_send_fname(cmd[1])))
+			sess->printfChan(sess, "master",
+				"Cannot send file "
+				"\"%s\"",
+				cmd[1]);
 		else
 			ret = true;
 	}
-	free_array((void **) cmd);
+	free_array((void **)cmd);
 	return (ret);
 }
